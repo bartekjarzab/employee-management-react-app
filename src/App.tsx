a5b4ac55dfd4fe23b82ced206ff81263c0c77980
Bartosz Jarząb <bartosz.jarzab99@gmail.com>
2022-06-30 22:51:22 +0200

    front app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,91 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import './App.css';
+import DepartmentAdd from './components/DepartmentAdd';
+import DepartmentsList from './components/DepartmentsList';
+import EmployeeAdd from './components/EmployeeAdd';
+import EmployeesList from './components/EmployeesList';
+import UpdateDepartment from './components/UpdateDeparment';
+import UpdateDepartments from './components/UpdateDepartments';
+import UpdateEmployee from './components/UpdateEmployee';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+       
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/"} className="nav-link">
+              Employees List
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/employeesAdd"} className="nav-link">
+              Employees Add
+            </Link>
+          </li>
+           <li className="nav-item">
+            <Link to={"/departmentsList"} className="nav-link">
+              Departments List
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/departmentsAdd"} className="nav-link">
+              Departments Add
+            </Link>
+          </li>
+           <li className="nav-item">
+            <Link to={"/updateDepartment"} className="nav-link">
+              Update Department
+            </Link>
+          </li>
+           <li className="nav-item">
+            <Link to={"/updateEmployee"} className="nav-link">
+              Update Employee
+            </Link>
+          </li>
+           <li className="nav-item">
+            <Link to={"/updateDepartments"} className="nav-link">
+              Update Departments
+            </Link>
+          </li>
+        </div>
+      </nav>
+        <Routes>
+          <Route path="/" 
+                 element={
+                    <EmployeesList/>
+                 } />
+                 <Route path="/employeesAdd" 
+                 element={
+                    <EmployeeAdd/>
+                 } />
+                 <Route path="/departmentsList" 
+                 element={
+                    <DepartmentsList/>
+                 } />
+                 <Route path="/departmentsAdd" 
+                 element={
+                    <DepartmentAdd/>
+                 } />
+                 <Route path="/updateDepartments" 
+                 element={
+                    <UpdateDepartments/>
+                 } />
+                  <Route path="/updateDepartment" 
+                 element={
+                    <UpdateDepartment/>
+                 } />
+                  <Route path="/updateEmployee" 
+                 element={
+                    <UpdateEmployee/>
+                 } />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
